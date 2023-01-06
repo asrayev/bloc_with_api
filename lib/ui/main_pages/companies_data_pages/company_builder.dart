94b54ffce36b06ddb141aec9dfeb65cb28cb63af
Abdurauf <raufasrayev1@gmail.com>
2023-01-06 14:13:02 +0500

    widget

--- lib/ui/main_pages/companies_data_pages/company_builder.dart
+++ lib/ui/main_pages/companies_data_pages/company_builder.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../bloc/company/company_cubit.dart';
+import '../../../bloc/company/company_state.dart';
 
 class WithBuilderPage extends StatelessWidget {
   const WithBuilderPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+   return Scaffold(
+
+        body:Center(
+          child: Column(
+            children: [
+              SizedBox(height: 200,),
+              BlocBuilder<CompanyCubit,CompanyState>(
+                builder: (context, state) {
+                  if(state is InitialCompanyState){
+                    return Text("Area of your rectangle is : ");
+                  }
+                  if(state is LoadCompanyInProgress){
+                    return Text("Area of your circle is : ");
+                  }
+                  if(state is LoadCompanyInSuccess){
+                    return Text("Area of your square is :${state.companies.data.length} ");
+                  }
+                  return const Text("Tap the button") ;
+                },
+
+              ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+              //   children: [
+              //     TextButton(onPressed: (){
+              //       BlocProvider.of<AreaCalcCubit>(context)
+              //           .calcRectArea(12, 12);
+              //     }, child: const Text("Rectangle")),
+              //     TextButton(onPressed: (){
+              //       BlocProvider.of<AreaCalcCubit>(context)
+              //           .calcCircleArea(12, 12);
+              //
+              //     }, child: const Text("Circle")),
+              //     TextButton(onPressed: (){
+              //       BlocProvider.of<AreaCalcCubit>(context)
+              //           .calcSquareArea(12, 12);
+              //     }, child: const  Text("Square")),
+              //   ],
+              // )
+
+            ],
+          ),
+        )
+
+    );
   }
 }
--- lib/ui/main_pages/companies_data_pages/company_consumer_page.dart
+++ lib/ui/main_pages/companies_data_pages/company_consumer_page.dart
@@ -22,40 +22,79 @@ class WithConsumerPage extends StatelessWidget {
         //         icon: const Icon(Icons.upload))
         //   ],
         // ),
-        body: BlocConsumer<CompanyCubit, CompanyState>(
-          builder: (context, state) {
-            if (state is InitialCompanyState) {
-              return const Center(
-                child: Text("Hali data yo'q"),
-              );
-            } else if (state is LoadCompanyInProgress) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (state is LoadCompanyInSuccess) {
-              return ListView(
-                children: List.generate(
-                  state.companies.data.length,
-                      (index) => ListTile(
-                    title: Text(
-                      state.companies.data[index].carModel,
-                    ),
+        body: SafeArea(
+          child: BlocConsumer<CompanyCubit, CompanyState>(
+            builder: (context, state) {
+              if (state is InitialCompanyState) {
+                return const Center(
+                  child: Text("Not Data"),
+                );
+              } else if (state is LoadCompanyInProgress) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is LoadCompanyInSuccess) {
+                return Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width*0.95,
+                    child: WidgetListView(state),
                   ),
-                ),
-              );
-            } else if (state is LoadCompanyInFailure) {
-              return Center(
-                child: Text(
-                  state.errorText,
-                ),
-              );
-            }
-            return const SizedBox();
-          },
-          listener: (context, state) {
-            if (state is LoadCompanyInProgress) {}
-          },
+                );
+                // state.companies.data[index].carModel,
+                // ,
+              } else if (state is LoadCompanyInFailure) {
+                return Center(
+                  child: Text(
+                    state.errorText,
+                  ),
+                );
+              }
+              return const SizedBox();
+            },
+            listener: (context, state) {
+              if (state is LoadCompanyInProgress) {}
+            },
+          ),
         )
     );
+  }
+
+  ListView WidgetListView(LoadCompanyInSuccess state) {
+    return ListView.builder(
+                      itemCount: state.companies.data.length,
+                      itemBuilder: (context, index){
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: Container(
+                        height: MediaQuery.of(context).size.height*0.1,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(15)
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 10, left: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                  height: MediaQuery.of(context).size.height*0.05,
+                                  child: Image.network(state.companies.data[index].logo)),
+
+                              Text(state.companies.data[index].carModel, style: TextStyle(color: Colors.white),),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(state.companies.data[index].averagePrice.toString(), style: TextStyle(color: Colors.white),),
+                                  Text(state.companies.data[index].establishedYear.toString()),
+                                ],
+                              ),
+
+                            ],
+                          ),
+                        ),
+
+                      ),
+                    );
+                  });
   }
 }
